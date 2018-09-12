579ec257bef59e86e2e2da75935fe0cb3a5a63b5
Sergey Reshetnikov <resh.sersh@gmail.com>
2018-09-13 03:11:30 +0700

    add

--- py_projects/taverns/taverns/pipelines.py
+++ py_projects/taverns/taverns/pipelines.py
@@ -14,17 +14,14 @@ class TavernsPipeline(object):
     def __init__(self):
         clinet = pymongo.MongoClient("localhost", 27017)
         db = clinet["tavern"]
-        self.PhRes = db["shashlikoff_tavern"]
+        self.collection = db["shashlikoff_tavern"]
         idx = IndexModel([('base_url', ASCENDING)], unique=True)
-        self.PhRes.create_indexes([idx])
-        # if your existing DB has duplicate records, refer to:
-
-    # https://stackoverflow.com/questions/35707496/remove-duplicate-in-mongodb/35711737
+        self.collection.create_indexes([idx])
 
     def process_item(self, item, spider):
         if isinstance(item, TavernsItem):
             try:
-                self.PhRes.update_one({'base_url': item['base_url']}, {'$set': dict((i, item[i]) for i in item if i != 'base_url')}, upsert=True)
+                self.collection.update_one({'base_url': item['base_url']}, {'$set': dict((i, item[i]) for i in item if i != 'base_url')}, upsert=True)
             except Exception:
                 pass
 
